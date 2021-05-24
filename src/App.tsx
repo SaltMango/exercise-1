6b495169a006dc4bb12a6431c694c0b6178911f0
Sreenath Maniyeri <mvsreenath@gmail.com>
2021-05-24 16:59:51 +0530

    add snapshot test

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,20 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 
 const app:React.FC = () => {
-  const [date, setDate] = useState<Date>(new Date());
+  const [date, setDate] = useState<number>(Date.now());
 
   useEffect(() => {
-    let timer: NodeJS.Timeout = setInterval(() => setDate(new Date()), 1000);
+    let timer: NodeJS.Timeout = setInterval(() => setDate(Date.now()), 1000);
     return () => {
       clearInterval(timer);
     };
   });
 
-  let time:string = date.toLocaleString("en-US", {
+  let time: string = new Date(date).toLocaleString("en-US", {
     hour12: true,
     hour: "2-digit",
     minute: "numeric",
-    second:  "numeric",
+    second: "numeric",
   });
   return (
     <div className="container">
--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -2,6 +2,7 @@ import App from "../App";
 import React from "react";
 import TestRenderer from "react-test-renderer";
 
+Date.now = jest.fn(() => new Date("2020-05-25T12:20:30Z").getTime());
 test("time renders correctly", () => {
   const tree = TestRenderer.create(<App />).toJSON();
   expect(tree).toMatchSnapshot();
